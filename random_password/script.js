70831ea01f351b98bfce266c94c2f862b9ec2ab8
Joachim Ray R. Chiong <131645680+JoachimRay@users.noreply.github.com>
2024-06-03 09:20:56 +0800

    Add files via upload

---
+++ random_password/script.js
@@ -0,0 +1,9 @@
+const button= document.getElementById("random");
+
+
+
+
+button.addEventListener("click", function(){
+    document.getElementById('loader').style.display = 'block';
+    button.style.display='none';
+});
